661f09931ee7320dace38bbc82ad18f9078efa55
Saikrishna Bonagiri <saikrishna.bonagiri@gmail.com>
2017-01-08 17:10:27 -0500

    first commit

---
+++ GeoQuiz/app/src/main/java/com/example/mayurmovaliya/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package com.example.mayurmovaliya.geoquiz;
+
+/**
+ * Created by Mayur Movaliya on 6/3/2016.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
+
